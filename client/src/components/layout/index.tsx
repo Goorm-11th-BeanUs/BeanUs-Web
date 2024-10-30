b125e1455a71b988c0484c3221abbf2f94917440
NonamedBread <lgh778923@gmail.com>
2024-10-31 08:27:04 +0900

    feat: vite, tsconfig, route 설정 추가

---
+++ client/src/components/layout/index.tsx
@@ -0,0 +1,60 @@
+// import Navbar from '@/components/layout/navbar'
+// import Sidebar from '@/components/layout/sidebar'
+// import PageLoading from '@/components/loading/PageLoading'
+// import { useAuth } from '@/hooks/useAuth'
+// import { useSidebar } from '@/hooks/useSidebar'
+// import { LOGIN_PATH } from '@/routes/Routes'
+// import { loginUserRoleState } from '@/store/login'
+// import { Box, Stack } from '@mui/material'
+import { Box, Stack } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+// import { useRecoilValue } from 'recoil'
+
+const AuthLayout = () => {
+  const isOpenSideBar = false // TODO: sidebar 있을 경우에만 true로 변경
+  // const { isOpenSideBar, toggleSideBar } = useSidebar()
+  // const { isLogin, onLogOut } = useAuth()
+  // const loginUserRole = useRecoilValue(loginUserRoleState)
+  // const location = useLocation()
+
+  // // 로그인 이동
+  // if (!isLogin) {
+  //   return <Navigate to={LOGIN_PATH} state={{ from: location }} />
+  // }
+
+  // if (!loginUserRole) {
+  //   return <PageLoading />
+  // }
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flex: '1 1 auto',
+          maxWidth: '100%',
+          paddingTop: 64,
+          paddingLeft: isOpenSideBar ? 280 : 0,
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            // width: '70%',
+            minWidth: '1024px',
+            margin: '0 auto',
+            padding: '32px',
+            paddingBottom: '120px',
+          }}
+        >
+          <Stack spacing={3} sx={{ width: '100%' }}>
+            <Outlet key={location.pathname} />
+          </Stack>
+        </Box>
+      </div>
+      {/* <Navbar onLogOut={onLogOut} isOpenSideBar={isOpenSideBar} toggleSideBar={toggleSideBar} />
+      <Sidebar loginUserRole={loginUserRole} /> */}
+    </>
+  )
+}
+export default AuthLayout
---
+++ client/src/routes/Routes.tsx
@@ -0,0 +1,116 @@
+// import AuthLayout from '@/components/layout'
+
+// 아이콘 검색: https://mui.com/material-ui/material-icons/
+// import { ADMIN_ROLE } from '@/constants/AdminType'
+import { getRoutesByRole } from '../constants/Navigator'
+// import { POST_TYPE } from '@/constants/PostType'
+import { createBrowserRouter } from 'react-router-dom'
+import AuthLayout from '../components/layout'
+import Main from '../page/Main'
+import NotFound from '../page/NotFound'
+
+// import LoginPage from '@/page/Login'
+// import NotFound from '@/page/NotFound'
+// import AdminCreate from '@/page/admin/Create'
+// import AdminDetail from '@/page/admin/Detail'
+// import AdminList from '@/page/admin/List'
+// import BannerCreate from '@/page/banner/Create'
+// import BannerDetail from '@/page/banner/Detail'
+// import BannerList from '@/page/banner/List'
+// import CalcurateList from '@/page/calculate/List'
+// import EstimateList from '@/page/estimate/List'
+// import GuestCreate from '@/page/guest/Create'
+// import GuestDetail from '@/page/guest/Detail'
+// import GuestList from '@/page/guest/List'
+// import HomeManagerDetail from '@/page/homeManager/Detail'
+// import HomeManagerList from '@/page/homeManager/List'
+// import HomeManagerReviewDetail from '@/page/homeManager/ReviewDetail'
+// import HomeManagerCreateWrapper from '@/page/homeManager/components/HomeManagerCreateWrapper'
+// import HotelCreate from '@/page/hotel/Create'
+// import HotelDetail from '@/page/hotel/Detail'
+// import HotelList from '@/page/hotel/List'
+// import MemberDetail from '@/page/member/Detail'
+// import MemberList from '@/page/member/List'
+// import MyPage from '@/page/mypage/MyPage'
+// import PointDetail from '@/page/point/Detail'
+// import PointList from '@/page/point/List'
+// import PostCreate from '@/page/post/Create'
+// import PostDetail from '@/page/post/Detail'
+// import PostList from '@/page/post/List'
+// import ProductCreate from '@/page/product/Create'
+// import ProductDetail from '@/page/product/Detail'
+// import ProductList from '@/page/product/List'
+// import ReservationDetail from '@/page/reservation/Detail'
+// import ReservationList from '@/page/reservation/List'
+// import ReviewCreate from '@/page/reviews/Create'
+// import ReviewDetail from '@/page/reviews/Detail'
+// import ReviewList from '@/page/reviews/List'
+// import TermsDetail from '@/page/terms/Detail'
+// import TermsList from '@/page/terms/List'
+// import BorderColorIcon from '@mui/icons-material/BorderColor'
+// import CalculateRoundedIcon from '@mui/icons-material/CalculateRounded'
+// import CheckIcon from '@mui/icons-material/Check'
+// import ContactSupportRoundedIcon from '@mui/icons-material/ContactSupportRounded'
+// import DashboardIcon from '@mui/icons-material/Dashboard'
+// import FlightIcon from '@mui/icons-material/Flight'
+// import HailIcon from '@mui/icons-material/Hail'
+// import HolidayVillageIcon from '@mui/icons-material/HolidayVillage'
+// import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
+// import NotificationsIcon from '@mui/icons-material/Notifications'
+// import PeopleIcon from '@mui/icons-material/People'
+// import ReceiptLongRoundedIcon from '@mui/icons-material/ReceiptLongRounded'
+// import ScreenshotMonitorIcon from '@mui/icons-material/ScreenshotMonitor'
+// import TableViewIcon from '@mui/icons-material/TableView'
+
+export const INDEX_PATH = '/'
+export const LOGIN_PATH = '/login'
+export const MYPAGE_PATH = '/mypage'
+
+export const ADMIN_ROLE = 'ADMIN'
+
+/** 페이지 메뉴 */
+export const PAGE_ROUTES: any[] = [
+  {
+    id: 'HOME',
+    path: INDEX_PATH,
+    index: true,
+    element: <Main />,
+    handle: {
+      name: '홈',
+      // icon: () => <DashboardIcon />,
+    },
+  },
+  {
+    id: 'MYPAGE',
+    path: MYPAGE_PATH,
+    element: <>My Page</>,
+    hidden: true,
+    handle: {
+      name: '마이페이지',
+    },
+  },
+]
+
+export const router = (role: string) => {
+  const pageRoutes = getRoutesByRole(role)
+
+  return createBrowserRouter([
+    {
+      path: LOGIN_PATH,
+      element: <>Login</>,
+    },
+    {
+      element: <AuthLayout />,
+      children: pageRoutes,
+    },
+    {
+      path: '*',
+      element: <AuthLayout />,
+      children: [
+        {
+          element: <NotFound />,
+        },
+      ],
+    },
+  ])
+}
