f87fb0799777d0d07c9161ba48ea1f0bdf35a8ef
NonamedBread <lgh778923@gmail.com>
2024-10-31 19:27:53 +0900

    feat: LocalizationProvider, DayPicker 추가, 패키지 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from '@mui/material/styles'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
@@ -7,8 +9,10 @@ import { theme } from './theme/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </LocalizationProvider>
   </StrictMode>
 )
--- client/src/page/collect/index.tsx
+++ client/src/page/collect/index.tsx
@@ -1,86 +1,45 @@
-import AccessTimeIcon from '@mui/icons-material/AccessTime'
-import EventIcon from '@mui/icons-material/Event'
-import { Box, Button, FormControl, TextField, Typography } from '@mui/material'
+import { Box, FormControl, Typography } from '@mui/material'
+import { TimePicker } from '@mui/x-date-pickers'
+import { useId, useState } from 'react'
+import DayPicker from '../../components/common/DayPicker'
+
+const initialState = {
+  cafe_id: '',
+  collect_days: [],
+  amount: 0,
+}
 
 const Collect = () => {
+  const [form, setForm] = useState({
+    ...initialState,
+    cafe_id: useId(),
+  })
+
+  console.log('Form:', form)
+
+  const handleDayChange = (selectedDays: number[]) => {
+    console.log('Selected days:', selectedDays)
+  }
+
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      height="100%"
-      p={2}
-      sx={{ border: '1px solid #e0e0e0', borderRadius: '4px' }}
-    >
-      <Box margin="auto" width="100%" display="flex" justifyContent="flex-end" mb={2}>
-        <Button
-          variant="contained"
-          sx={{ backgroundColor: '#4caf50', '&:hover': { backgroundColor: '#388e3c' } }}
-          startIcon={<EventIcon />}
-        >
-          수거 현황
-        </Button>
+    <Box display="flex" flexDirection="column" height="100%" p={4} gap={4}>
+      <Box
+        margin="auto"
+        width="100%"
+        display="flex"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Typography variant="h2">수거 정보 수정</Typography>
       </Box>
-      <Box display="flex" flex={1}>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-          width="50%"
-          sx={{ gap: 4, p: 2 }}
-        >
-          <Box display="flex" sx={{ gap: 4 }}>
-            <Button
-              variant="contained"
-              sx={{ backgroundColor: '#2196f3', '&:hover': { backgroundColor: '#1976d2' } }}
-              startIcon={<AccessTimeIcon />}
-            >
-              30분 뒤
-            </Button>
-            <Button
-              variant="contained"
-              sx={{ backgroundColor: '#2196f3', '&:hover': { backgroundColor: '#1976d2' } }}
-              startIcon={<AccessTimeIcon />}
-            >
-              1시간 뒤
-            </Button>
-          </Box>
-          <Box display="flex" sx={{ gap: 4 }} flexDirection="column" alignItems="center">
-            <Typography variant="h6">커피 찌꺼기 무게</Typography>
-            <FormControl required>
-              <TextField
-                id="conceptId"
-                type="number"
-                label="무게(g)"
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                      borderColor: '#ff9800',
-                    },
-                    '&:hover fieldset': {
-                      borderColor: '#fb8c00',
-                    },
-                    '&.Mui-focused fieldset': {
-                      borderColor: '#f57c00',
-                    },
-                  },
-                }}
-              />
-            </FormControl>
-          </Box>
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-          width="50%"
-          p={2}
-        >
-          <Typography variant="h6">수거 요청 날짜</Typography>
-        </Box>
-      </Box>
+      <FormControl sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Typography variant="h4">수거 요일을 모두 선택해주세요.</Typography>
+        <DayPicker onChange={handleDayChange} />
+      </FormControl>
+      <FormControl sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Typography variant="h4">수거 시간을 선택해 주세요.</Typography>
+        <TimePicker onChange={newValue => console.log('Selected time:', newValue)} />
+      </FormControl>
     </Box>
   )
 }
